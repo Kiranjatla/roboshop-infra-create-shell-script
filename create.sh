43aaf6f508822982c0b0ab7c2a0c4e6be0b641a4
KIRAN <ks@gmail.com>
2025-03-24 19:28:51 +0530

    create instance

--- create.sh
+++ create.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ##### Change these values ###(Zone id means Route53 Hosted Zone id,Security group id is in security groups)
-ZONE_ID="Z0041272APBG07XRTPCJ"
+ZONE_ID="Z07170951UQPFBKT6XC20"
 SG_NAME="allow-all"
 #ENV="dev"
 #############################
